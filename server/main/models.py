a05fce8f243da5af2f074d96cb18e65a625b16dc
ThisIsTAEWON <125544913+ThisIsTAEWON@users.noreply.github.com>
2024-11-25 21:21:40 +0900

    GET update

--- server/main/models.py
+++ server/main/models.py
@@ -5,3 +5,16 @@ class PlantData(models.Model):
     temperature = models.CharField(max_length=10)
     humidity = models.CharField(max_length=10)
     illuminance = models.CharField(max_length=10)
+
+    # def __init__(self, soil_moisture, temperature, humidity, illuminance):
+    #     self.soil_moisture = soil_moisture
+    #     self.temperature = temperature
+    #     self.humidity = humidity
+    #     self.illuminance = illuminance
+
+    def dict_data(self):
+        di = {"soil_moisture" : self.soil_moisture,
+                "temperature" : self.temperature,
+                "humidity" : self.humidity,
+                "illuminance" : self.illuminance,}
+        return di
--- server/main/views.py
+++ server/main/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import PlantData
 import json
 
 # index.html 페이지를 부르는 index 함수
 def index(request):
-    return render(request, 'main/index.html')
+    datalist = PlantData.objects.all()
+    return render(request, 'main/index.html', {"datalist": datalist})
 
 def update(request):
-    body = json.loads(request.body.decode('utf-8'))
-    data = PlantData.objects.create(
-        soil_moisture = body["plantSoilMoisture"],
-        temperature = body["plantTemperature"],
-        humidity = body["plantHumidity"],
-        illuminance = body["plantIlluminance"],
-    )
-    # return render(request, 'main/index.html', {'data': data})
-    return "success: " + body
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+
+        data = PlantData.objects.first()
+
+        data.soil_moisture = body["plantSoilMoisture"]
+        data.temperature = body["plantTemperature"]
+        data.humidity = body["plantHumidity"]
+        data.illuminance = body["plantIlluminance"]
+
+        data.save()
+        
+        # init regacy
+        # data = PlantData.objects.create(
+        #     soil_moisture = 0,
+        #     temperature = 0,
+        #     humidity = 0,
+        #     illuminance = 0,
+        # )
+
+        # return render(request, 'main/index.html', {'data': data})
+        return HttpResponse(json.dumps(body))
+    else:
+        data = PlantData.objects.first().dict_data()
+        return HttpResponse(json.dumps(data))
